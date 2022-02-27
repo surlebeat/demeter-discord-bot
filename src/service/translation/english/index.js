09f36133668dfe545488d249f57294ccbdb96d52
ootsun <gabriel.delhaye@hotmail.com>
2022-02-27 18:58:22 +0100

    Fix Captcha display

--- src/service/translation/english/index.js
+++ src/service/translation/english/index.js
@@ -1,6 +1,6 @@
 export const MESSAGE = {
     'Try again...\n\n': 'Try again...\n\n',
-    'Please select the emoji you see in the image': (successQty) =>`Please select the emoji you see in the image(${successQty}/3):`,
+    'Please select the emoji you see in the image': (successQty) =>`Please select the emoji you see in the image (${successQty+1}/3):`,
     'Well done!\n': 'Well done!\n',
     'You are now verified.': 'You are now verified.'
 }
